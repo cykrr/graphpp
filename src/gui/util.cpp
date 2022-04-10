a4256e9ad70fd939996e8de5f8e896da2569a782
Your Name <you@example.com>
2022-04-10 09:13:58 -0400

    def clear hex

---
+++ src/gui/util.cpp
@@ -0,0 +1,13 @@
+#include "gui/util.hpp"
+namespace gl {
+    
+void clearColorHex(std::string fmt)
+{
+    int b,c,d;
+    sscanf(fmt.c_str(), "#%02x%02x%02x", &b, &c, &d);
+    glClearColor((float)b/0xff,
+            (float)c/0xff, 
+            (float)d/0xff, 
+            1.0f);
+}
+}
