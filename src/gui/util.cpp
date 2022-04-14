c14a89b6689c5ecbf9ed79d9a1cff9997443688b
Your Name <you@example.com>
2022-04-14 03:49:01 -0400

    fix indent and add time defs

--- src/gui/util.cpp
+++ src/gui/util.cpp
@@ -1,13 +1,22 @@
 #include "gui/util.hpp"
 namespace gl {
-    
-void clearColorHex(std::string fmt)
-{
-    int b,c,d;
-    sscanf(fmt.c_str(), "#%02x%02x%02x", &b, &c, &d);
-    glClearColor((float)b/0xff,
-            (float)c/0xff, 
-            (float)d/0xff, 
-            1.0f);
+    void clearColorHex(std::string fmt)
+    {
+        int b,c,d;
+        sscanf(fmt.c_str(), "#%02x%02x%02x", &b, &c, &d);
+        glClearColor((float)b/0xff,
+                (float)c/0xff, 
+                (float)d/0xff, 
+                1.0f);
+    }
 }
+namespace Time {
+    float dt = 0.f;
+    long now = 0.f;
+    long then = 0.f;
+    void update() {
+        now = vkfw::getTime();
+        dt = now - then; 
+        then = now;
+    }
 }
