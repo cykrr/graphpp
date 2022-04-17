401a0c6f7fdf17cbac529a13f801c59cc318af2d
Your Name <you@example.com>
2022-04-17 09:26:09 -0400

    add vertex functions, sort by vars and funcs

--- include/gui/plane.hpp
+++ include/gui/plane.hpp
@@ -2,16 +2,22 @@
 #define PLANE_H
 #include "gui/program.hpp"
 class Plane {
-    public:
-        Plane();
+public:
+    Plane();
+    // unprojected rectangle vertices: (-1 -1 to 1 1)
+    static float vertices[18];
+    void draw();
 
     // we can't use the cube bc they use different vertices!
     // well we "can" but let's first use this plane..
     unsigned int Vao, Vbo;
-    // unprojected rectangle vertices:
-    static float vertices[18];
-
     static Program *program;
+private:
+
+    void createBuffers();
+    void bindBuffers();
+    void enableAttribArray();
+    void allocateVertexData();
 
 };
 #endif
